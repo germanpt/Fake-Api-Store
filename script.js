0fb5205b65df66ed1a90ee4aa86371226dd40f82
Mohamed Ibrahim <40779184+germanpt@users.noreply.github.com>
2025-10-31 23:28:29 +0200

    Cart

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Script Loaded");
+const cart = [];
 async function loadProducts() {
   try {
     const res = await fetch("https://fakestoreapi.com/products");
@@ -10,7 +11,7 @@ async function loadProducts() {
     container.innerHTML = data
       .map(
         (product) => `
-        <div class="product-card" data-id="${product.id} data-title="${product.title}" data-price="${product.price}" >
+        <div class="product-card" data-id="${product.id}" data-title="${product.title}" data-price="${product.price}" >
           <img src="${product.image}" alt="${product.title}" class="product-image" />
           <div class="product-name">${product.title}</div>
           <div class="product-price">$${product.price}</div>
@@ -28,8 +29,45 @@ async function loadProducts() {
   }
 }
 loadProducts();
+
+// Adding event listeners to buttons that were rendered dynamically //
 function addToCartEvents() {
   document.querySelectorAll(".btn-cart").forEach((btn) => {
-    btn.addEventListener("click");
+    btn.addEventListener("click", () => {
+      const productElement = btn.closest(".product-card");
+      const product = {
+        id: +productElement.dataset.id,
+        title: productElement.dataset.title,
+        price: +productElement.dataset.price,
+      };
+      addItemToCart(product);
+    });
   });
+}
+
+// Add to cart Logic
+
+function addItemToCart(product) {
+  const exists = cart.some((item) => item.id === product.id);
+  if (!exists) {
+    cart.push(product);
+    console.log("item added");
+  } else {
+    alert("Iteam already in your cart");
+  }
+  displayCartItems();
+}
+function displayCartItems() {
+  const cartContainer = document.querySelector(".shopping-cart");
+  cartContainer.innerHTML = `
+    <h2>Shopping Cart</h2>
+    ${cart
+      .map(
+        (
+          item
+        ) => `<div>${item.title} - $${item.price} <button class="remove-item"> Remove </button></div>
+    `
+      )
+      .join("")}
+  `;
 }
